14bc06fc720060847631dcd7c3f0518b587a47d8
yevhen-arkhipov <100130149+yevhen-arkhipov@users.noreply.github.com>
2022-06-13 22:31:50 +0000

    Deploying to gh-pages from @ MaksymFrankov/IceCream@9e912afc98c68c5debe0ff5efa5997d68b99027c 🚀

---
+++ index.10caa17c.js
@@ -0,0 +1,2 @@
+function scrollFunction(){document.body.scrollTop>100||document.documentElement.scrollTop>100?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}mybutton=document.getElementById("scroll-up-button"),window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.10caa17c.js.map
