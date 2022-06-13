3554951cf1f8320bf1985b05ca445d5e2140f8d2
MaksymFrankov <55510261+MaksymFrankov@users.noreply.github.com>
2022-06-13 19:19:05 +0000

    Deploying to gh-pages from @ MaksymFrankov/IceCream@46c2d77c4c4d6f6f9aec70a0b79b48f691e50338 🚀

--- index.06794c58.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu")};function n(){e.menu.classList.toggle("visually-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.06794c58.js.map
