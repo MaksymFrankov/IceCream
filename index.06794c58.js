6269c81f3a33a760bc3b53d2fa830489170004bc
MaksymFrankov <55510261+MaksymFrankov@users.noreply.github.com>
2022-06-13 18:55:01 +0000

    Deploying to gh-pages from @ MaksymFrankov/IceCream@ae3a2b526228f3cbaee247da9725cbb36943ecc7 🚀

--- index.06794c58.js
+++ index.06794c58.js
@@ -1,2 +1,2 @@
 !function(){var e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu")};function n(){e.menu.classList.toggle("visually-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.3e37dc66.js.map
+//# sourceMappingURL=index.06794c58.js.map
--- index.90b77512.js
+++ index.90b77512.js
@@ -1,2 +1,2 @@
 function readMore(){var e=document.getElementById("about__dots"),n=document.getElementById("about__btn"),t=document.getElementById("about__more");"none"===e.style.display?(e.style.display="inline",n.innerHTML="Open",t.style.display="none"):(e.style.display="none",n.style.display="none",t.style.display="inline")}
-//# sourceMappingURL=index.65a71e0f.js.map
+//# sourceMappingURL=index.90b77512.js.map
