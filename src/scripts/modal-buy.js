8208b42f13e8e44dc3e546a5cdf79547e7bf139f
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2022-06-14 21:50:54 +0300

    пофиксил javasript

--- src/scripts/modal-buy.js
+++ src/scripts/modal-buy.js
@@ -1,22 +1,9 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-buy-open]"),
+    openModalBtn: document.querySelector("[data-mob-open]"),
     closeModalBtn: document.querySelector("[data-buy-close]"),
     modal: document.querySelector("[data-buy]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-mob-open]"),
-    modal: document.querySelector("[data-mob]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
